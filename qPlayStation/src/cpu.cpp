3f4e6478c06bbba7de917be3bf6878fb9cc0a139
quinnpainter <quinn@geekymonkey.com>
2020-03-19 13:31:25 +0000

    Basic instruction decoding + LUI and ORI instructions

--- qPlayStation/src/cpu.cpp
+++ qPlayStation/src/cpu.cpp
@@ -9,6 +9,23 @@ cpu::cpu(memory* mem)
 void cpu::reset()
 {
 	pc = 0xBFC00000;
+	for (int i = 0; i < 32; i++)
+	{
+		regs[i] = 0;
+	}
+	hi = 0;
+	lo = 0;
+}
+
+void cpu::setReg(int index, uint32_t value)
+{
+	regs[index] = value;
+	regs[0] = 0;
+}
+
+uint32_t cpu::getReg(int index)
+{
+	return regs[index];
 }
 
 void cpu::step()
@@ -20,5 +37,105 @@ void cpu::step()
 
 void cpu::executeInstr(uint32_t instr)
 {
-	logging::fatal("Instr " + helpers::intToHex(instr), logging::logSource::CPU);
+	switch (decode_op(instr))
+	{
+		case 0x00: logging::fatal("Unimplemented instruction: Special" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x01: logging::fatal("Unimplemented instruction: BcondZ" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x02: logging::fatal("Unimplemented instruction: J" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x03: logging::fatal("Unimplemented instruction: JAL" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x04: logging::fatal("Unimplemented instruction: BEQ" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x05: logging::fatal("Unimplemented instruction: BNE" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x06: logging::fatal("Unimplemented instruction: BLEZ" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x07: logging::fatal("Unimplemented instruction: BGTZ" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x08: logging::fatal("Unimplemented instruction: ADDI" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x09: logging::fatal("Unimplemented instruction: ADDIU" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x0A: logging::fatal("Unimplemented instruction: SLTI" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x0B: logging::fatal("Unimplemented instruction: SLTIU" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x0C: logging::fatal("Unimplemented instruction: ANDI" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x0D: op_ori(instr); break;
+		case 0x0E: logging::fatal("Unimplemented instruction: XORI" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x0F: op_lui(instr); break;
+		case 0x10: logging::fatal("Unimplemented instruction: COP0" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x11: logging::fatal("Unimplemented instruction: COP1" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x12: logging::fatal("Unimplemented instruction: COP2" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x13: logging::fatal("Unimplemented instruction: COP3" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x20: logging::fatal("Unimplemented instruction: LB" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x21: logging::fatal("Unimplemented instruction: LH" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x22: logging::fatal("Unimplemented instruction: LWL" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x23: logging::fatal("Unimplemented instruction: LW" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x24: logging::fatal("Unimplemented instruction: LBU" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x25: logging::fatal("Unimplemented instruction: LHU" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x26: logging::fatal("Unimplemented instruction: LWR" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x28: logging::fatal("Unimplemented instruction: SB" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x29: logging::fatal("Unimplemented instruction: SH" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x2A: logging::fatal("Unimplemented instruction: SWL" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x2B: logging::fatal("Unimplemented instruction: SW" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x2E: logging::fatal("Unimplemented instruction: SWR" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x30: logging::fatal("Unimplemented instruction: LWC0" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x31: logging::fatal("Unimplemented instruction: LWC1" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x32: logging::fatal("Unimplemented instruction: LWC2" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x33: logging::fatal("Unimplemented instruction: LWC3" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x38: logging::fatal("Unimplemented instruction: SWC0" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x39: logging::fatal("Unimplemented instruction: SWC1" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x3A: logging::fatal("Unimplemented instruction: SWC2" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		case 0x3B: logging::fatal("Unimplemented instruction: SWC3" + helpers::intToHex(instr), logging::logSource::CPU); break;
+		default: logging::fatal("Invalid instruction: " + helpers::intToHex(instr), logging::logSource::CPU); break;
+	}
+}
+
+// -------------------------- Instruction Decode Helpers --------------------------
+
+uint8_t cpu::decode_op(uint32_t instr) // Main opcode identifier
+{
+	return instr >> 26;
+}
+
+uint8_t cpu::decode_rs(uint32_t instr) // Source Register
+{
+	return (instr >> 21) & 0x1F;
+}
+
+uint8_t cpu::decode_rt(uint32_t instr) // Target Register
+{
+	return (instr >> 16) & 0x1F;
+}
+
+uint8_t cpu::decode_rd(uint32_t instr) // Destination Register
+{
+	return (instr >> 11) & 0x1F;
+}
+
+uint16_t cpu::decode_imm(uint32_t instr) // Immediate Value
+{
+	return instr & 0xFF;
+}
+
+uint8_t cpu::decode_shamt(uint32_t instr) // Shamt (??)
+{
+	return (instr >> 6) & 0x1F;
+}
+
+uint8_t cpu::decode_funct(uint32_t instr) // Funct (used for secondary opcode decoding)
+{
+	return instr & 0x3F;
+}
+
+uint32_t cpu::decode_target(uint32_t instr) // 26 bit jump target address
+{
+	return instr & 0x3FFFFFF;
+}
+
+// -------------------------- Instructions --------------------------
+
+void cpu::op_lui(uint32_t instr) // Load Upper Immediate
+{
+	// Set upper 16 bits of a reg to the 16 bit immediate value (lower bits are 0)
+	setReg(decode_rt(instr), ((uint32_t)decode_imm(instr)) << 16);
+}
+
+void cpu::op_ori(uint32_t instr) // Or Immediate
+{
+	// Set lower 16 bits of a reg to another reg bitwise OR an immediate value
+	uint32_t output = getReg(decode_rs(instr)) | decode_imm(instr);
+	setReg(decode_rt(instr), output);
 }
--- qPlayStation/src/cpu.hpp
+++ qPlayStation/src/cpu.hpp
@@ -11,6 +11,24 @@ class cpu
 	private:
 		uint32_t pc;
 		uint32_t regs[32];
+		uint32_t hi;
+		uint32_t lo;
 		memory* Memory = nullptr;
 		void executeInstr(uint32_t instr);
+		void setReg(int index, uint32_t value);
+		uint32_t getReg(int index);
+
+		// Instruction Decode Helpers
+		uint8_t decode_op(uint32_t instr);
+		uint8_t decode_rs(uint32_t instr);
+		uint8_t decode_rt(uint32_t instr);
+		uint8_t decode_rd(uint32_t instr);
+		uint16_t decode_imm(uint32_t instr);
+		uint8_t decode_shamt(uint32_t instr);
+		uint8_t decode_funct(uint32_t instr);
+		uint32_t decode_target(uint32_t instr);
+
+		// Instructions
+		void op_lui(uint32_t instr);
+		void op_ori(uint32_t instr);
 };
